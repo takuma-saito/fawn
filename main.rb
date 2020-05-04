d035ba2e76ac30b4460b9f7ec3577d22a5f8c206
saito <saito@saitonoMacBook-Air.local>
2020-05-05 05:25:27 +0900

    shutdown 関係追加した

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ module MiniWebServer
     @logger ||= Logger.new(STDOUT)
   end
   class ThreadPool
-    def initialize(max, min = max)
+    def initialize(max, min = max/2)
       @mutex = Mutex.new
       @empty = ConditionVariable.new
       @full  = ConditionVariable.new
@@ -18,18 +18,19 @@ module MiniWebServer
       @spawned = 0
       @running = 0
       @shutdown = false
+      @graceful_shutdown = false
       @workers = []
       @min.times do
         sync { spawn_thread }
       end
     end
-    def new_worker
+    def new_worker(initial_job)
       proc do |id|
         Thread.current[:name] = id
         catch do |tag|
+          job = initial_job
           loop do
             begin
-              job = nil
               sync do
                 while @jobs.empty?
                   @empty.wait(@mutex) if @jobs.size == 0
@@ -40,7 +41,8 @@ module MiniWebServer
               end
               Timeout.timeout(TIMEOUT) { job.(id) }
               sync do
-                throw tag if @shutdown
+                throw tag if (@shutdown && !@graceful_shutdown) ||
+                             (@graceful_shutdown && @jobs.empty?)
                 @running -= 1
                 @full.signal
               end
@@ -54,20 +56,33 @@ module MiniWebServer
         logger.info "worker #{id} is terminated"
       end
     end
-    def spawn_thread
-      @workers << Thread.new(@spawned += 1, &new_worker)
+    def spawn_thread(job = nil)
+      @workers << Thread.new(@spawned += 1, &new_worker(job))
     end
     def sync
       @mutex.synchronize { yield }
     end
     def push(job)
+      return if @shutdown 
       sync do
-        @full.wait(@mutex) if (@running + @jobs.size) > @max
+        is_full = (@running + @jobs.size) > @max
+        @full.wait(@mutex) if is_full
         @jobs << job
-        @empty.signal
+        (@jobs.size - 1 - (@spawned - @running)) > 0 && !is_full ?
+          spawn_thread(@jobs.shift) : @empty.signal
       end
     end
     alias :<< :push
+    def graceful_shutdown
+      sync do
+        @graceful_shutdown = true
+        logger.info "stat: {job_size:%2d, running:%2d, workers:%2d}" % [
+                      @jobs.size,
+                      @running,
+                      @workers.size
+                    ]
+      end
+    end
     def shutdown
       sync do
         @shutdown = true
@@ -84,9 +99,9 @@ module MiniWebServer
 end
 
 include MiniWebServer
-tp = ThreadPool.new(12)
+tp = ThreadPool.new(4)
 
-35.times do |id|
+10.times do |id|
   puts "job add: #{id}"
   tp << proc do |worker_id|
     puts "start {worker_id:#{worker_id}, job_id:#{id}}"
