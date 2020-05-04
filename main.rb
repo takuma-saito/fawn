106b6e5051057bfa994fe4635f2a3988e53f247d
saito <saito@saitonoMacBook-Air.local>
2020-05-05 03:34:19 +0900

    調整した

--- main.rb
+++ main.rb
@@ -16,6 +16,7 @@ module MiniWebServer
       @spawned = 0
       @running = 0
       @shutdown = false
+      @workers = []
     end
     def new_worker
       proc do |id|
@@ -44,22 +45,34 @@ module MiniWebServer
       end
     end
     def spawned
-      Thread.new(@spawned += 1, &new_worker)
+      @workers << Thread.new(@spawned += 1, &new_worker)
+      @full.wait(@mutex)
     end
     def sync
       @mutex.synchronize { yield }
     end
     def push(job)
       sync do
-        @full.wait(@mutex) if (@running + @jobs.size) > @max
         @jobs << job
         spawned if (@jobs.size - @running) > 0
-        @empty.signal
+        logger.info "stat: {job_size:#{@jobs.size}, running:#{@running}}"
+        @empty.broadcast
+        @full.wait(@mutex) if (@running + @jobs.size) >= @max
       end
     end
     alias :<< :push
+    def graceful_shutdown
+      sync {
+        @shutdown = true
+        @empty.broadcast
+        @workers.each(&:join)
+      }
+    end
     def shutdown
-      @empty.broadcast # TODO: running のプロセスの終了を待つにはどうしたらいいか
+      sync {
+        @shutdown = true
+        @empty.broadcast # TODO: running のプロセスの終了を待つにはどうしたらいいか
+      }
     end
   end
 end
@@ -67,14 +80,15 @@ end
 include MiniWebServer
 tp = ThreadPool.new(3)
 
-5.times do |id|
+10.times do |id|
+  puts "job add: #{id}"
   tp << proc do |worker_id|
-    puts "start {worker_id:#{worker_id}, id:#{id}}"
+    puts "start {worker_id:#{worker_id}, job_id:#{id}}"
     sleep (rand * 2.0)
-    puts "finish {worker_id:#{worker_id}, id:#{id}}"
+    puts "finish {worker_id:#{worker_id}, job_id:#{id}}"
   end
 end
 
-sleep 5.0
-tp.shutdown
+# sleep 8.0
+tp.graceful_shutdown
 
